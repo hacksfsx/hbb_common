89dd04ea7d6643410d0dd055fe342dd309eafa91
hacksfsx <hacksfsx@163.com>
2026-07-07 12:27:46 +0800

    update rendezvous server and pub key

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["111.231.33.159"];
+pub const RS_PUB_KEY: &str = "kqlERCdph7szPfaRBjlzIuNBsRQNDkW538mHbyqjrbw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
